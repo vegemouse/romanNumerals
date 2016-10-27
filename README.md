0b290dd64125dfadbf8c173ab8180d4d959f4970
Lina Shadrach and Maxwell Cady <lina@>
2016-10-27 12:02:17 -0700

    create page that takes decimal numers and converts them to roman numerals

---
+++ README.md
@@ -0,0 +1,15 @@
+### return an alert asking for numbers if non-numerical characters are entered as input
+* 'abc'
+* 'abc' is not a number. Please enter a number.
+### return an alert asking for a smaller number if a number greater than 3,999 is entered
+* 4000
+* Please enter a number that is less than 3,999.
+### divide the number entered by the Roman Numeral (RN) that factor into it, starting with the greatest factor and progressing to the least and return the string of Roman Numerals
+* 3999
+* MMMDCCCCLXXXXVIIII
+### if the number is divisible by a Roman Numeral factor more than three times, it will convert that instance of the RN to read as subtraction
+* 3999
+* MMMDCDIC
+### change only use the subtraction specification with RN that are related by a factor of 10
+* 3999
+* MMMDCDXCIX
---
+++ js/scripts.js
@@ -0,0 +1,133 @@
+var checkSize = function(number){
+  if (number > 3999){
+    return false;
+  }
+  else {
+    return true;
+  }
+}
+var checkNumber = function(decNum) {
+  var parsedNum = parseFloat(decNum.replace(/,/g, ""));
+  if (parsedNum === NaN) {
+    alert(decNum + " is not a number. Please enter a number.");
+  }
+  else {
+    checkSize(Math.round(parsedNum));
+    if (checkSize === false) {
+      alert(decNum + " is too big! Try again.")
+    }
+    else {
+      return romanNumeralizer(Math.round(parsedNum));
+    }
+  }
+};
+
+var getLength = function(number) {
+  return number.toString().length;
+}
+
+var romanNumeralizer = function(number) {
+  var rnM = 0;
+  var rnD = 0;
+  var rnC = 0;
+  var rnL = 0;
+  var rnX = 0;
+  var rnV = 0;
+  var rnI = 0;
+  var length = getLength(number);
+  if (length === 4) {
+    while (number > 999){
+      number = number - 1000;
+      rnM++;
+    };
+    length = getLength(number);
+  }
+  if (length === 3) {
+    while (number > 499){
+      number = number - 500;
+      rnD++;
+    }
+    length = getLength(number);
+    if (length === 3) {
+      while (number > 99) {
+        number = number - 100;
+        rnC++;
+      }
+      length = getLength(number);
+    }
+  }
+  if (length === 2) {
+    while (number > 49){
+      number = number - 50;
+      rnL++;
+    }
+    length = getLength(number);
+    if (length === 2) {
+      while (number > 9) {
+        number = number - 10;
+        rnX++;
+      }
+      length = getLength(number);
+    }
+  }
+  console.log("n" + number);
+  if (length === 1) {
+    while (number > 4){
+      number = number - 5;
+      rnV++;
+    };
+    length = getLength(number);
+    if (length === 1) {
+      while (number > 0) {
+        number = number - 1;
+        rnI++;
+      }
+    }
+  }
+  var rnAmts = [rnM, rnD, rnC, rnL, rnX, rnV, rnI];
+  console.log(rnAmts);
+  var rnArr = ["M", "D", "C", "L", "X", "V", "I"];
+  var outputArr = [];
+  var rn = "";
+  var convert9 = false;
+  var convert4 = false;
+  for (i = 0; i < rnAmts.length; i++) {
+    if (rnAmts[i] === 4 && rnAmts[i-1] > 0){
+      rnAmts[i] = 1;
+      outputArr.pop();
+      convert9 = true;
+      console.log(convert9);
+    }
+    if (rnAmts[i] === 4){
+      rnAmts[i] = 1;
+      outputArr.pop();
+      convert4 = true;
+      console.log(convert4);
+    }
+    for (j = 0; j < rnAmts[i]; j++) {
+      outputArr.push(rnArr[i]);
+    }
+    if (convert9 === true){
+      outputArr.push(rnArr[i-2]);
+      convert9 = false;
+    }
+    if (convert4 === true){
+      outputArr.push(rnArr[i-1]);
+      convert4 = false;
+    }
+  }
+  output = outputArr.join("");
+  console.log("f" + output);
+  return output;
+};
+
+$(function() {
+  $("form#rn").submit(function(event) {
+
+    var inputDec = $("#decimal").val();
+    var rnAnswer = checkNumber(inputDec);
+    console.log(rnAnswer);
+    $("#roman").append(rnAnswer);
+    event.preventDefault();
+  })
+})
